3fe2e294c72ce82da431758b2e410cd88cb370bd
Matthijs217 <matthijstenbrink@gmail.com>
2024-11-19 14:11:25 +0100

    dropdown werkend, Titel van header aangepast

--- scripts/main-page.js
+++ scripts/main-page.js
@@ -1,5 +1,11 @@
 const dropDown = document.querySelector('.dropdownarrow');
 
 dropDown.addEventListener('click', () => {
-   console.log('clicked');
+    if (dropDown.style.transform === 'rotate(180deg)') {
+        // Zet hem terug naar normaal
+        dropDown.style.transform = 'rotate(0deg)';
+      } else {
+        // Draai hem 180 graden
+        dropDown.style.transform = 'rotate(180deg)';
+      }
 });
