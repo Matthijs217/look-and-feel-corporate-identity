d978437f465b3542a9787652bced306232d3d1a2
Matthijs217 <matthijstenbrink@gmail.com>
2024-11-20 11:43:21 +0100

    header af #1
    #1

--- scripts/main-page.js
+++ scripts/main-page.js
@@ -1,12 +1,19 @@
 const dropDown = document.querySelector('.dropdownarrow');
+const header = document.querySelector('header'); // Selecteer de header
+const switchRouwTaken = document.querySelector('.switchrouwtaken'); // Selecteer het element
 
 dropDown.addEventListener('click', () => {
     if (dropDown.style.transform === 'rotate(180deg)') {
         // Zet hem terug naar normaal
         dropDown.style.transform = 'rotate(0deg)';
-      } else {
+        header.classList.remove('header2'); // Verwijder de nieuwe class van de header
+        header.classList.add('header'); // Voeg de originele class weer toe
+        switchRouwTaken.style.display = 'none'; // Verberg switchRouwTaken
+    } else {
         // Draai hem 180 graden
         dropDown.style.transform = 'rotate(180deg)';
-      }
+        header.classList.remove('header'); // Verwijder de originele class van de header
+        header.classList.add('header2'); // Voeg de nieuwe class toe
+        switchRouwTaken.style.display = 'flex'; // Toon switchRouwTaken
+    }
 });
-
